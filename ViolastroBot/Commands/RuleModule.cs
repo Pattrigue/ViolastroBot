b64751cacab3650280d7be19f700881b5f5b0bb9
Pattrigue <patrick@gamehouse.dk>
2023-09-02 15:30:18 +0200

    Create RuleModule.cs

---
+++ ViolastroBot/Commands/RuleModule.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+using Discord;
+using Discord.Commands;
+using Discord.Interactions;
+using Discord.WebSocket;
+
+namespace ViolastroBot.Commands;
+
+public sealed partial class RuleModule : ModuleBase<SocketCommandContext>
+{
+    [Command("rule")]
+    [Discord.Commands.Summary("Display the rule with the specified number in chat.")]
+    [RequireRole(Roles.Moderator)]
+    public Task SendRule(string ruleNumberText)
+    {
+        SocketTextChannel rulesChannel = Context.Guild.GetTextChannel(Channels.Rules);
+        IMessage rulesMessage = rulesChannel.GetMessagesAsync(1).FlattenAsync().Result.First();
+        
+        string[] rulesMessageLines = rulesMessage.Content.Split('\n');
+        
+        foreach (string line in rulesMessageLines)
+        {
+            if (Regex().IsMatch(line) && line.StartsWith(ruleNumberText))
+            {
+                return ReplyAsync(line);
+            }
+        }
+
+        return ReplyAsync($"Rule {ruleNumberText} ain't a thing!!");
+    }
+
+    [GeneratedRegex("^[0-9].+$")]
+    private static partial Regex Regex();
+}
