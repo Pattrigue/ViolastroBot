377c1f871e11aa2d4047dd66ed11c45a0c2c55f9
Pattrigue <patrick@gamehouse.dk>
2023-09-02 15:21:32 +0200

    Create WarnModule.cs

---
+++ ViolastroBot/Commands/WarnModule.cs
@@ -0,0 +1,23 @@
+﻿using Discord.Commands;
+using Discord.Interactions;
+using Discord.WebSocket;
+
+namespace ViolastroBot.Commands;
+
+public sealed class WarnModule : ModuleBase<SocketCommandContext>
+{
+    [Command("warn")]
+    [Discord.Commands.Summary("Warns the mentioned user and gives them the warning role.")]
+    [RequireRole(Roles.Moderator)]
+    public Task WarnUser([Remainder] string _ = "")
+    {
+        if (Context.Message.MentionedUsers.Count == 0) return Task.CompletedTask;
+
+        SocketGuildUser user = Context.Guild.GetUser(Context.Message.MentionedUsers.First().Id);
+        SocketRole warningRole = Context.Guild.GetRole(Roles.Warning);
+        
+        user.AddRoleAsync(warningRole);
+        
+        return ReplyAsync($"You have been given the warning role for misbehaving {user.Mention}. Please follow the server rules.");
+    }
+}
