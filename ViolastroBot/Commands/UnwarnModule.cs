5101f6cc45d09604e960de34861e256d172964d6
Pattrigue <patrick@gamehouse.dk>
2023-09-02 15:22:43 +0200

    Create UnwarnModule.cs

---
+++ ViolastroBot/Commands/UnwarnModule.cs
@@ -0,0 +1,23 @@
+﻿using Discord.Commands;
+using Discord.Interactions;
+using Discord.WebSocket;
+
+namespace ViolastroBot.Commands;
+
+public sealed class UnwarnModule : ModuleBase<SocketCommandContext>
+{
+    [Command("unwarn")]
+    [Discord.Commands.Summary("Removes the warning role from the mentioned user.")]
+    [RequireRole(Roles.Moderator)]
+    public Task UnwarnUser([Remainder] string _ = "")
+    {
+        if (Context.Message.MentionedUsers.Count == 0) return Task.CompletedTask;
+
+        SocketGuildUser user = Context.Guild.GetUser(Context.Message.MentionedUsers.First().Id);
+        SocketRole warningRole = Context.Guild.GetRole(Roles.Warning);
+        
+        if (user.Roles.All(role => role.Id != Roles.Warning)) return Task.CompletedTask;
+        
+        return user.RemoveRoleAsync(warningRole);
+    }
+}
