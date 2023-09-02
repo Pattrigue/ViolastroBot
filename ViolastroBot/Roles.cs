3765cb55f815bda4af2489e4f7d49bdc7cf77fd0
Pattrigue <patrick@gamehouse.dk>
2023-09-02 15:21:31 +0200

    Update Roles.cs

--- ViolastroBot/Roles.cs
+++ ViolastroBot/Roles.cs
@@ -4,4 +4,5 @@ public static class Roles
 {
     public const ulong Birthday = 771066239740411905;
     public const ulong Moderator = 687660687463415808;
+    public const ulong Warning = 744280694829875280;
 }
