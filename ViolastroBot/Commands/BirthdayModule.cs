f59819bec81efd275d8dfc272ef03240cfcd27fb
Pattrigue <patrick@gamehouse.dk>
2023-09-02 14:14:25 +0200

    Create BirthdayModule.cs

---
+++ ViolastroBot/Commands/BirthdayModule.cs
@@ -0,0 +1,29 @@
+﻿using Discord.Commands;
+using Discord.Interactions;
+using Discord.WebSocket;
+
+namespace ViolastroBot.Commands;
+
+public sealed class BirthdayModule : ModuleBase<SocketCommandContext>
+{
+    [Command("bday")]
+    [Discord.Commands.Summary("Assigns the birthday role to the the mentioned user.")]
+    [RequireRole(Roles.Moderator)]
+    public Task AssignBirthdayRole([Remainder] string _ = "")
+    {
+        if (Context.Message.MentionedUsers.Count == 0) return Task.CompletedTask;
+
+        SocketGuildUser user = Context.Guild.GetUser(Context.Message.MentionedUsers.First().Id);
+        
+        SocketRole birthdayRole = Context.Guild.GetRole(Roles.Birthday);
+
+        if (user.Roles.Any(role => role.Id == Roles.Birthday))
+        {
+            return user.RemoveRoleAsync(birthdayRole);
+        }
+        
+        user.AddRoleAsync(birthdayRole);
+        
+        return ReplyAsync($"Happy dabby birthday, {user.Mention}! Bwehehe!!");
+    }
+}
