103e51a2fc9c40db3ef69e1816708e13223232fc
Pattrigue <patrick@gamehouse.dk>
2023-09-03 19:54:36 +0200

    Update Startup.cs

--- ViolastroBot/Startup.cs
+++ ViolastroBot/Startup.cs
@@ -27,7 +27,7 @@ public sealed class Startup
         JobScheduler jobScheduler = services.GetRequiredService<JobScheduler>();
         await jobScheduler.InitializeAsync(_client);
         await jobScheduler.ScheduleCronJob<RenameChannelJob>("0 0 * ? * *");
-        await jobScheduler.ScheduleCronJob<RemoveBirthdayRolesJob>("0 0 8 ? * *");
+        await jobScheduler.ScheduleCronJob<RemoveBirthdayRolesJob>("0 0 10 ? * *");
 
         await Task.Delay(-1);
     }
