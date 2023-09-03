77de3e0b268425ebdfc204d2a7659bd165b73855
Pattrigue <patrick@gamehouse.dk>
2023-09-03 19:30:24 +0200

    Create JobScheduler.cs

---
+++ ViolastroBot/QuartzJobs/JobScheduler.cs
@@ -0,0 +1,33 @@
+﻿using Discord.WebSocket;
+using Quartz;
+using Quartz.Impl;
+
+namespace ViolastroBot.QuartzJobs;
+
+public sealed class JobScheduler
+{
+    private IScheduler _scheduler;
+    
+    public async Task InitializeAsync(DiscordSocketClient client)
+    {
+        StdSchedulerFactory factory = new StdSchedulerFactory();
+        _scheduler = await factory.GetScheduler();
+        _scheduler.Context.Add("DiscordClient", client);
+        await _scheduler.Start();
+    }
+    
+    public async Task ScheduleCronJob<T>(string cronSchedule) where T : IJob
+    {
+        // Define the job and tie it to our MyJob class
+        IJobDetail job = JobBuilder.Create<T>().Build();
+                    
+        // Trigger the job to run now, and then every 40 seconds
+        ITrigger trigger = TriggerBuilder.Create()
+            .WithCronSchedule(cronSchedule) // Cron expression for every 1 minute
+            .StartNow()
+            .Build();
+                    
+        // Schedule the job using the job and trigger 
+        await _scheduler.ScheduleJob(job, trigger);
+    }
+}
