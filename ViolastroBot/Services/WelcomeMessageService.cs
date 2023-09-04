fc57e42691ac352f52ef1b7af97694f8dc6c44f7
Pattrigue <patrick@gamehouse.dk>
2023-09-04 22:05:11 +0200

    Create WelcomeMessageService.cs

---
+++ ViolastroBot/Services/WelcomeMessageService.cs
@@ -0,0 +1,23 @@
+﻿using Discord.WebSocket;
+using ViolastroBot.DiscordServerConfiguration;
+
+namespace ViolastroBot.Services;
+
+public sealed class WelcomeMessageService : ServiceBase
+{
+    private readonly DiscordSocketClient _client;
+
+    public WelcomeMessageService(DiscordSocketClient client)
+    {
+        _client = client;
+        _client.UserJoined += UserJoinedAsync;
+    }
+
+    private async Task UserJoinedAsync(SocketGuildUser user)
+    {
+        if (_client.GetChannel(Channels.GeneralChannel) is SocketTextChannel channel)
+        {
+            await channel.SendMessageAsync($"Welcome to the server, {user.Mention}!");
+        }
+    }
+}
