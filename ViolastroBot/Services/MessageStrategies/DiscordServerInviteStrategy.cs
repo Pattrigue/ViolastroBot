f39591044e87068f5b45cd48294c0cc303828ef8
Pattrigue <patrick@gamehouse.dk>
2023-09-04 21:36:52 +0200

    Update DiscordServerInviteStrategy.cs

--- ViolastroBot/Services/MessageStrategies/DiscordServerInviteStrategy.cs
+++ ViolastroBot/Services/MessageStrategies/DiscordServerInviteStrategy.cs
@@ -3,17 +3,33 @@ using Discord.WebSocket;
 
 namespace ViolastroBot.Services.MessageStrategies;
 
-public partial class DiscordServerInviteStrategy : IMessageStrategy
+public sealed partial class DiscordServerInviteStrategy : IMessageStrategy
 {
+    private static readonly HashSet<string> AllowedInvites = new(StringComparer.OrdinalIgnoreCase) { "SSUTPCU" };
+
     public async Task ExecuteAsync(SocketUserMessage message)
     {
-        if (DiscordInviteRegex().IsMatch(message.Content))
+        Match match = DiscordInviteRegex().Match(message.Content);
+
+        if (!match.Success)
         {
-            await message.DeleteAsync();
-            await message.Channel.SendMessageAsync("Please don't advertise y'all's Discord servers here!");
+            return;
         }
+
+        // Extract the invite code using the named group
+        string inviteCode = match.Groups["InviteCode"].Value;
+
+        // Check if the invite code is in the HashSet of allowed invites
+        if (AllowedInvites.Contains(inviteCode))
+        {
+            return;
+        }
+
+        // Not an allowed invite, so delete and warn
+        await message.DeleteAsync();
+        await message.Channel.SendMessageAsync("Please don't advertise y'all's Discord servers here!");
     }
 
-    [GeneratedRegex(@"(https:\/\/)?(www\.)?(discord\.gg|discord\.me|discordapp\.com\/invite|discord\.com\/invite)\/([a-z0-9-.]+)?", RegexOptions.IgnoreCase)]
+    [GeneratedRegex(@"(https:\/\/)?(www\.)?(discord\.gg|discord\.me|discordapp\.com\/invite|discord\.com\/invite)\/(?<InviteCode>[a-z0-9-.]+)", RegexOptions.IgnoreCase)]
     private static partial Regex DiscordInviteRegex();
 }
