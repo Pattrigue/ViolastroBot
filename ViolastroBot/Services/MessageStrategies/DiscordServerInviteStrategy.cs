f4c258b1d5868f440ea82446b46ca778a375c5ff
Pattrigue <patrick@gamehouse.dk>
2023-09-04 21:27:09 +0200

    Create DiscordServerInviteStrategy.cs

---
+++ ViolastroBot/Services/MessageStrategies/DiscordServerInviteStrategy.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+using Discord.WebSocket;
+
+namespace ViolastroBot.Services.MessageStrategies;
+
+public partial class DiscordServerInviteStrategy : IMessageStrategy
+{
+    public async Task ExecuteAsync(SocketUserMessage message)
+    {
+        if (DiscordInviteRegex().IsMatch(message.Content))
+        {
+            await message.DeleteAsync();
+            await message.Channel.SendMessageAsync("Please don't advertise y'all's Discord servers here!");
+        }
+    }
+
+    [GeneratedRegex(@"(https:\/\/)?(www\.)?(discord\.gg|discord\.me|discordapp\.com\/invite|discord\.com\/invite)\/([a-z0-9-.]+)?", RegexOptions.IgnoreCase)]
+    private static partial Regex DiscordInviteRegex();
+}
