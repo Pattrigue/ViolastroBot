d4fa2210f08afd8729f7b33d7dc9532784949148
Pattrigue <patrick@gamehouse.dk>
2023-09-08 22:23:11 +0200

    Update !roulette command

--- ViolastroBot/Commands/RouletteModule.cs
+++ ViolastroBot/Commands/RouletteModule.cs
@@ -2,6 +2,7 @@
 using Discord;
 using Discord.Commands;
 using ViolastroBot.Commands.Roulette;
+using ViolastroBot.DiscordServerConfiguration;
 
 namespace ViolastroBot.Commands;
 
@@ -16,8 +17,26 @@ public sealed class RouletteModule : ModuleBase<SocketCommandContext>
     private readonly Random _random = new();
     private readonly Dictionary<Type, RouletteAction> _rouletteActions;
     
+    private readonly List<string> _randomResponses = new()
+    {
+        "Deleting the server in 5 minutes...",
+        "I'm feeling full of beans!",
+        "We think it is good, but we design things on certaim things.",
+        "public static void main string args",
+        "I HATE USING GREEN TO THE SPIKES",
+        "I Can Fix you.",
+        "it requires... RNG?????????????????????????????????",
+        "Would U need sign a deal???.",
+        "( yep, DAILY! ]",
+        "https://tenor.com/view/boing-bounce-thingamabob-the-thingamabob-thing-gif-24819899",
+        "https://media.discordapp.net/attachments/1110626262784938025/1118087579754049586/image0-43.gif",
+        "https://media.discordapp.net/attachments/884162046772531200/902911462752808990/caption.gif",
+        "<:keoiki:1149796185553518713>",
+    };
+    
     public RouletteModule(IServiceProvider services)
     {
+        _randomResponses = _randomResponses.Concat(Jokes.List).ToList();
         _rouletteActions = Assembly.GetExecutingAssembly()
             .GetTypes()
             .Where(t => t.IsSubclassOf(typeof(RouletteAction)))
@@ -32,41 +51,77 @@ public sealed class RouletteModule : ModuleBase<SocketCommandContext>
     [Summary("Plays the roulette.")]
     public async Task PlayRoulette()
     {
-        if (Cooldowns.TryGetValue(Context.User.Id, out DateTimeOffset lastUsed))
+        if (await IsUserOnCooldown())
         {
-            int cooldownInMinutes = DefaultCooldownInMinutes;
-            
-            if (Context.Guild.GetUser(Context.User.Id).PremiumSince != null)
-            {
-                cooldownInMinutes = PremiumCooldownInMinutes;
-            }
-            
-            TimeSpan cooldown = TimeSpan.FromMinutes(cooldownInMinutes);
-            TimeSpan difference = DateTimeOffset.Now - lastUsed;
-
-            if (cooldown > difference)
-            {
-                TimeSpan waitTime = cooldown - difference;
-                
-                int minutes = waitTime.Minutes;
-                int seconds = waitTime.Seconds;
-
-                await ReplyAsync($"Ya gotta wait {minutes} minutes and {seconds} seconds before ya can play the roulette again!!");
-                return;
-            }
+            return;
         }
 
         Cooldowns[Context.User.Id] = DateTimeOffset.Now;
 
-        if (_random.Next(0, 100) > 50)
+        if (await UserLostRoulette())
         {
-            await Context.Message.AddReactionAsync(new Emoji("🇱"));
             return;
         }
 
         await ExecuteRandomRouletteAction();
     }
 
+    private async Task<bool> IsUserOnCooldown()
+    {
+        if (!Cooldowns.TryGetValue(Context.User.Id, out DateTimeOffset lastUsed))
+        {
+            return false;
+        }
+        
+        int cooldownInMinutes = DefaultCooldownInMinutes;
+
+        if (Context.Guild.GetUser(Context.User.Id).PremiumSince != null)
+        {
+            cooldownInMinutes = PremiumCooldownInMinutes;
+        }
+
+        TimeSpan cooldown = TimeSpan.FromMinutes(cooldownInMinutes);
+        TimeSpan difference = DateTimeOffset.Now - lastUsed;
+
+        if (cooldown <= difference)
+        {
+            return false;
+        }
+        
+        TimeSpan waitTime = cooldown - difference;
+
+        int minutes = waitTime.Minutes;
+        int seconds = waitTime.Seconds;
+
+        await ReplyAsync($"Ya gotta wait {minutes} minutes and {seconds} seconds before ya can play the roulette again!!");
+        
+        return true;
+    }
+    
+    private async Task<bool> UserLostRoulette()
+    {
+        if (_random.Next(0, 100) > 50)
+        {
+            if (_random.Next(0, 100) < 75)
+            {
+                await ReplyWithRandomResponse();
+            }
+            else
+            {
+                await Context.Message.AddReactionAsync(new Emoji("🇱"));
+
+                if (_random.Next(0, 100) < 20)
+                {
+                    await ReplyAsync("Womp womp!");
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+    
     private async Task ExecuteRandomRouletteAction()
     {
         double randomValue = _random.NextDouble() * 100;
@@ -79,7 +134,7 @@ public sealed class RouletteModule : ModuleBase<SocketCommandContext>
             _ => RouletteActionTier.VeryRare
         };
 
-        var actionsInSelectedTier = _rouletteActions
+        RouletteAction[] actionsInSelectedTier = _rouletteActions
             .Where(kvp => kvp.Key.GetCustomAttribute<RouletteActionTierAttribute>()?.Tier == selectedTier)
             .Select(kvp => kvp.Value)
             .ToArray();
@@ -95,5 +150,12 @@ public sealed class RouletteModule : ModuleBase<SocketCommandContext>
         Console.WriteLine($"Executing {selectedAction.GetType().Name}...");
 
         await selectedAction.ExecuteAsync(Context);
+    }
+
+    private Task ReplyWithRandomResponse()
+    {
+        string response = _randomResponses[new Random().Next(0, _randomResponses.Count)];
+
+        return ReplyAsync(response);
     }
 }
