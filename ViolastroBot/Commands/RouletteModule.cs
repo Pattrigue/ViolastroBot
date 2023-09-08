2d5acce00178ad8a136f2aad61a8e50fe088b87e
Pattrigue <patrick@gamehouse.dk>
2023-09-08 16:03:39 +0200

    Update RouletteModule.cs

--- ViolastroBot/Commands/RouletteModule.cs
+++ ViolastroBot/Commands/RouletteModule.cs
@@ -1,4 +1,5 @@
 ﻿using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
 using ViolastroBot.Commands.Preconditions;
 using ViolastroBot.DiscordServerConfiguration;
 using ViolastroBot.Extensions;
@@ -9,20 +10,27 @@ namespace ViolastroBot.Commands;
 
 public sealed class RouletteModule : ModuleBase<SocketCommandContext>
 {
-    private ILoggingService _logger;
+    private readonly IServiceProvider _services;
+    private readonly CommandService _commands;
+    private readonly ILoggingService _logger;
     
-    public RouletteModule(ILoggingService logger)
+    public RouletteModule(IServiceProvider services)
     {
-        _logger = logger;
+        _services = services;
+        _logger = services.GetRequiredService<ILoggingService>();
+        _commands = services.GetRequiredService<CommandService>();
     }
     
     [Command("roulette")]
     [RequireRole(Roles.Moderator)]
     public Task PlayRoulette()
     {
-        return SetUsernameToRandomWords();
+        return RunRandomCommand();
     }
 
+    /// <summary>
+    /// Sets the user's nickname to a random name.
+    /// </summary>
     private async Task SetUsernameToRandomWords()
     {
         List<string> words = new WordRandomizer().GetRandomWords(1, 3);
@@ -41,5 +49,21 @@ public sealed class RouletteModule : ModuleBase<SocketCommandContext>
         {
             await _logger.LogMessageAsync($"An unexpected error occurred: {ex.Message}");
         }
+    }
+
+    /// <summary>
+    /// Runs a random command that has no required roles and no required parameters.
+    /// </summary>
+    private async Task RunRandomCommand()
+    {
+        IEnumerable<CommandInfo> commands = _commands.Commands.Where(command =>
+        {
+            return command.Preconditions.All(precondition => precondition is not RequireRoleAttribute) 
+                   && (command.Parameters.Count == 0 || command.Parameters.All(p => p.IsOptional));
+        }).ToList();
+
+        CommandInfo randomCommand = commands.ElementAt(new Random().Next(0, commands.Count()));
+        
+        await _commands.ExecuteAsync(Context, randomCommand.Name, _services);
     }
 }
