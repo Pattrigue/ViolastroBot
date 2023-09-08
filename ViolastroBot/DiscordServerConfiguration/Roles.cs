e8c2c6c21a26262ba46512598f609047e7905d6b
Pattrigue <patrick@gamehouse.dk>
2023-09-08 20:18:36 +0200

    Update Roles.cs

--- ViolastroBot/DiscordServerConfiguration/Roles.cs
+++ ViolastroBot/DiscordServerConfiguration/Roles.cs
@@ -6,4 +6,5 @@ public static class Roles
     public const ulong Moderator = 687660687463415808;
     public const ulong Warning = 744280694829875280;
     public const ulong NewRole = 771077503682609172;
+    public const ulong LeadDeveloper = 315091073854734336;
 }
