d4004a9236925d16257c12218040f69fad8fa569
Pattrigue <patrick@gamehouse.dk>
2023-09-10 14:05:30 +0200

    Update AssignNewRole.cs

--- ViolastroBot/Commands/Roulette/Actions/AssignNewRole.cs
+++ ViolastroBot/Commands/Roulette/Actions/AssignNewRole.cs
@@ -24,29 +24,66 @@ public sealed class AssignNewRole : RouletteAction
     {
         SocketRole role = Context.Guild.GetRole(Roles.NewRole);
 
-        SocketGuildUser userWithRole = Context.Guild.Users.FirstOrDefault(user =>
-            user.Roles.Any(userRole => userRole.Id == Roles.NewRole));
+        List<SocketGuildUser> usersWithRole = Context.Guild.Users.Where(user =>
+            user.Roles.Any(userRole => userRole.Id == Roles.NewRole)).ToList();
 
         SocketGuildUser userToReceiveRole = Context.Guild.GetUser(Context.User.Id);
 
         StringBuilder reply = new StringBuilder();
 
-        if (userWithRole != null)
+        if (usersWithRole.Count > 0)
         {
-            if (userWithRole.Id == userToReceiveRole.Id)
+            // Check if the current user already has the role
+            if (usersWithRole.Any(u => u.Id == userToReceiveRole.Id))
             {
                 await ReplyAsync($"Erm!! This is awkward... Ya see, I was gonna give ya the {role.Mention} role, but ya already have it! Bwehehe!!");
                 return;
             }
 
-            reply.AppendLine($"That means {userWithRole.Mention} no longer has it - too bad!");
-            await userWithRole.RemoveRoleAsync(Roles.NewRole);
+            await RemoveRoleFromCurrentUsersWithRole(usersWithRole, role, reply);
         }
 
-        await userToReceiveRole.AddRoleAsync(Roles.NewRole);
-        reply.Insert(0, $"Bwehehe!! {userToReceiveRole.Mention} now has the {role.Mention} role! ");
+        // Add role to the current user
+        await userToReceiveRole.AddRoleAsync(role);
+        reply.Insert(0, $"Bwehehe!! {userToReceiveRole.Mention} now has the {role.Mention} role!{Environment.NewLine}");
 
+        // Assuming _scoreboardService.IncrementScoreboardAsync is a method you have for some scoreboard functionality
         await _scoreboardService.IncrementScoreboardAsync(Context.Guild, Context.User);
-        await ReplyAsync(reply.ToString());
+        await Context.Channel.SendMessageAsync(reply.ToString(), allowedMentions: AllowedMentions.None);
+    }
+
+    private static async Task RemoveRoleFromCurrentUsersWithRole(
+        List<SocketGuildUser> usersWithRole,
+        IRole role,
+        StringBuilder reply)
+    {
+        List<string> mentions = new List<string>();
+
+        foreach (SocketGuildUser user in usersWithRole)
+        {
+            mentions.Add(user.Mention);
+            await user.RemoveRoleAsync(role);
+        }
+
+        string mentionString;
+        string haveOrHas;
+
+        switch (mentions.Count)
+        {
+            case 1:
+                mentionString = mentions[0];
+                haveOrHas = "has";
+                break;
+            case 2:
+                mentionString = $"{mentions[0]} and {mentions[1]}";
+                haveOrHas = "have";
+                break;
+            default:
+                mentionString = string.Join(", ", mentions.Take(mentions.Count - 1)) + ", and " + mentions.Last();
+                haveOrHas = "have";
+                break;
+        }
+
+        reply.AppendLine($"That means {mentionString} no longer {haveOrHas} it - too bad!");
     }
 }
