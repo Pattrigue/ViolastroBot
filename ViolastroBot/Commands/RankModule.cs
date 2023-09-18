5d7da3ed0fbd51612674e24a79bf393291f256b6
Pattrigue <patrick@gamehouse.dk>
2023-09-18 11:17:09 +0200

    Update RankModule.cs

--- ViolastroBot/Commands/RankModule.cs
+++ ViolastroBot/Commands/RankModule.cs
@@ -9,19 +9,33 @@ public sealed class RankModule : ModuleBase<SocketCommandContext>
 {
     [Command("rank")]
     [Summary("Displays the user's rank for the month.")]
-    public Task DisplayRank()
+    public Task DisplayRank([Remainder] string text = null)
     {
         DateTime date = DateTime.Now;
         int year = date.Year;
         int month = date.Month;
 
-        ulong seed = Context.User.Id + (ulong)month + (ulong)year;
+        ulong id;
+        string rankText;
+        
+        if (!string.IsNullOrEmpty(text) && text.ToLowerInvariant() == "bot")
+        {
+            id = Context.Client.CurrentUser.Id;
+            rankText = "My rank";
+        }
+        else
+        {
+            id = Context.User.Id;
+            rankText = "Rank";
+        }
+
+        ulong seed = id + (ulong)month + (ulong)year;
 
         WordRandomizer wordRandomizer = new(seed);
         
         List<string> words = wordRandomizer.GetRandomWords(1, 3);
         words[0] = words[0].CapitalizeFirstCharacter();
         
-        return ReplyAsync($"Rank: {string.Join(' ', words)}.");
+        return ReplyAsync($"{rankText}: {string.Join(' ', words)}.");
     }
 }
