30d22ee66b75a7612de574cfae8c5842e7e4d7d6
Pattrigue <patrick@gamehouse.dk>
2023-09-19 20:28:40 +0200

    Update CommandHandlerService.cs

--- ViolastroBot/Services/CommandHandlerService.cs
+++ ViolastroBot/Services/CommandHandlerService.cs
@@ -54,6 +54,9 @@ public sealed class CommandHandlerService : ServiceBase
 
         // Execute the command with the command context we just
         // created, along with the service provider for precondition checks.
-        await _commands.ExecuteAsync(context: context, argPos: argPos, services: _services);
+        _ = Task.Run(async () =>
+        {
+            await _commands.ExecuteAsync(context: context, argPos: argPos, services: _services);
+        });
     }
 }
