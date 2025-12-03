5cabecd77320ad3eebd5a0555b2a97d086fd4e53
Pattrigue <patrick@gamehouse.dk>
2025-12-03 20:25:16 +0100

    Refactor rank command date and string comparison

--- ViolastroBot/Features/Commands/RankModule.cs
+++ ViolastroBot/Features/Commands/RankModule.cs
@@ -11,14 +11,12 @@ public sealed class RankModule : ModuleBase<SocketCommandContext>
     [Summary("Displays the user's rank for the month.")]
     public Task DisplayRank([Remainder] string text = null)
     {
-        var date = DateTime.Now;
-        var year = date.Year;
-        var month = date.Month;
+        var (year, month, _) = DateTime.Now;
 
         ulong id;
         string rankText;
 
-        if (!string.IsNullOrEmpty(text) && text.ToLowerInvariant() == "bot")
+        if (!string.IsNullOrEmpty(text) && text.Equals("bot", StringComparison.InvariantCultureIgnoreCase))
         {
             id = Context.Client.CurrentUser.Id;
             rankText = "My rank";
